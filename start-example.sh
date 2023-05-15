b2fdd394fb10f4da2b48bd467ea230b2c9231b91
Sam <8408317+samgabrail@users.noreply.github.com>
2023-05-15 18:01:20 +0000

    add start example and install dependencies

--- start-example.sh
+++ start-example.sh
@@ -2,14 +2,14 @@
 URL="https://{YOUR_HOSTNAME}.ngrok.io"
 SECRET="{YOUR_RANDOM_STRING}"
 TOKEN="{YOUR_TOKEN}"
-USERNAME="{the username of your GitHub, GitLab or Bitbucket user}"
-REPO_ALLOWLIST="$YOUR_GIT_HOST/$YOUR_USERNAME/$YOUR_REPO" #GitLab example: REPO_ALLOWLIST="gitlab.com/public-projects3/infrastructure-software-public/env0-atlantis"
+USERNAME="samgabrail"
+REPO_ALLOWLIST="github.com/samgabrail/env0-atlantis-gcp" #GitHub example: REPO_ALLOWLIST="github.com/runatlantis/atlantis"
 # Any environment variables required for auth
 export GOOGLE_APPLICATION_CREDENTIALS="path-to-json-file"
 
 atlantis server \
 --atlantis-url="$URL" \
---gitlab-user="$USERNAME" \
---gitlab-token="$TOKEN" \
---gitlab-webhook-secret="$SECRET" \
+--gh-user="$USERNAME" \
+--gh-token="$TOKEN" \
+--gh-webhook-secret="$SECRET" \
 --repo-allowlist="$REPO_ALLOWLIST"
