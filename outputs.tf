c76c0a70cf3812cd1c649ccca8c3b29d650e5963
tekanaid <37753095+tekanaid@users.noreply.github.com>
2023-05-16 15:22:18 +0000

    remove hostname

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "instance_id" {
 output "public_ip" {
   value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
-
-output "hostname" {
-  value = google_compute_instance.default.hostname
-}
